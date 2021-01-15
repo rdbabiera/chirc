ca0d81d41d933a3dbb909afb10ce3a042a787ec4
RD Babiera <rdbabiera@cs.uchicago.edu>
2021-01-14 23:20:35 -0600

    fixed libraries in main

--- src/main.c
+++ src/main.c
@@ -43,6 +43,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "log.h"
 
